fa0618a2691968de8528bdc52717ccaf359ef6fa
Evan Cutler Anway <eca7372@uncw.edu>
2020-09-19 11:26:29 -0400

    import dough data

--- source/dough.R
+++ source/dough.R
@@ -2,3 +2,12 @@
 # 9/19/2020
 # ECA
 
+if(!require(pacman)) install.packages("pacman"); library(pacman)
+
+# Import ####
+p_load(
+  readxl, 
+  here
+)
+
+df_dough <- read_excel(here::here("working", "data", "dough.xlsx"))
