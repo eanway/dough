e2479583fadc778fec97e1ee9a855fac16032e98
Evan Cutler Anway <eca7372@uncw.edu>
2020-09-19 13:04:10 -0400

    rename durations, set data types, move column names to vector, rename intermediate dataframes

--- source/dough.R
+++ source/dough.R
@@ -29,40 +29,38 @@ shift_day <- function(early_hour, late_hour) {
 }
 
 standardize_rating <- function(rating, min, max) {
-  rating_int <- rating %>%
+  rating %>%
     recode(
       "yes" = as.character(max), 
       "no" = as.character(min)
     ) %>%
     as.integer()
-  
-  (rating_int - min) / (10 - min)
 }
 
-df_dough_standard <- df_dough %>%
+vec_core <- c(
+  "batch_number", 
+  "final_dough_temperature", 
+  "room_temperature", 
+  "humidity", 
+  "hydration", 
+  "leaven_percentage", 
+  "units", 
+  "rating"
+)
+
+df_dough_thin <- df_dough %>%
   clean_names() %>%
   select(
-    batch_number, 
+    all_of(vec_core), 
     day, 
-    final_dough_temperature, 
-    room_temperature, 
-    humidity, 
-    hydration, 
-    leaven_percentage, 
-    auto_hour, 
-    bulk_hour, 
-    bench_hour, 
-    proof_hour, 
-    bake_hour, 
-    bake_finish,
-    units, 
-    mill_date, 
-    rating
+    ends_with(c("hour", "finish", "date"))
   ) %>%
   filter(
     !is.na(day)
-  ) %>%
-  # standardize dates
+  )
+
+# Standardize dates ####
+df_dough_dates <- df_dough_thin %>%
   mutate(
     mill_date = as.Date(as.integer(mill_date), origin = "1899-12-30")
   ) %>%
@@ -75,20 +73,54 @@ df_dough_standard <- df_dough %>%
     proof_hour = shift_day(bench_hour, proof_hour), 
     bake_hour = shift_day(proof_hour, bake_hour), 
     bake_finish = shift_day(bake_hour, bake_finish)
-  ) %>%
-  # standardize ratings
+  )
+
+# standardize ratings ####
+df_dough_rating <- df_dough_dates %>%
   mutate(
-    rating_new = standardize_rating(rating, 0, 10)
+    rating = standardize_rating(rating, 0, 10)
   )
 
 
 # Analyze ####
-df_dough_analysis <- df_dough_standard %>%
+df_dough_duration <- df_dough_rating %>%
   # not including fermentation duration because it's the sum of bulk, bench, and proof
   mutate(
-    auto_duration = difftime(bulk_hour, auto_hour, units = "mins"), 
-    bulk_duration = difftime(bench_hour, bulk_hour, units = "mins"), 
-    bench_duration = difftime(proof_hour, bench_hour, units = "mins"), 
-    proof_duration = difftime(bake_hour, proof_hour, units = "mins"), 
-    bake_duration = difftime(bake_finish, bake_hour, units = "mins")
+    auto_duration_mins = difftime(bulk_hour, auto_hour, units = "mins"), 
+    bulk_duration_mins = difftime(bench_hour, bulk_hour, units = "mins"), 
+    bench_duration_mins = difftime(proof_hour, bench_hour, units = "mins"), 
+    proof_duration_mins = difftime(bake_hour, proof_hour, units = "mins"), 
+    bake_duration_mins = difftime(bake_finish, bake_hour, units = "mins")
+  ) %>%
+  mutate(
+    flour_age_days = difftime(day, mill_date, units = "days")
+  ) %>%
+  select(
+    all_of(vec_core), 
+    ends_with("duration_mins"), 
+    flour_age_days
   )
+
+
+# Set Data Types ####
+df_dough_standard <- df_dough_duration %>%
+  mutate(
+    across(c("batch_number"), as.factor), 
+    across(
+      c("final_dough_temperature", "room_temperature", 
+        "humidity", "hydration", "leaven_percentage"), 
+      as.numeric
+    ), 
+    across(c(where(is.difftime), "units", "rating"), as.integer)
+  )
+
+summary(df_dough_standard)
+
+
+# Export ####
+p_load(
+  readr
+)
+
+write_rds(df_dough_standard, here::here("working", "out", "df_dough_standard.rds"))
+write_csv(df_dough_standard, here::here("working", "out", "df_dough_standard.csv"), na = "")
